69aba15fdddabbc352cf4b31938e42735be1755f
weiyi_gisl002 <wc2302@columbia.edu>
2012-09-05 11:02:20 -0400

    update splineMI.h

--- src/splineMI.h
+++ src/splineMI.h
@@ -21,6 +21,7 @@ double entropy1(const double*, int, int);
 
 // export R function
 void mi2(const double *, const double *, int *, int *, int *, double *, int *, int *);
+void mi3(const double *, const double *, const double *, int *, int *, int *, double*);
 void mi2DiffBins(const double *, const double *, int *, int *, int *, int *, int *, double *, int *, int *);
 void mi2vs1(const double *, const double *, const double *, int *, int *, int *, double *, int *);
 void getAllMIWz(const double *, const double* , double *, int *, int *, int *, int *, int *, int *);
