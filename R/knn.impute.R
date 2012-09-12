82061d3aa4f09c9e3a9654f6099dec6282b52319
weiyi_gisl002 <wc2302@columbia.edu>
2012-09-12 17:45:16 -0400

    packageStartupMessage

--- R/knn.impute.R
+++ R/knn.impute.R
@@ -46,7 +46,7 @@ ewknn.impute <- function(X, wvec = rep(1, nrow(X)), k=11){
 	X[idx] = out
 }
 
-ewknn.predict <- function(X, t, qX, wvec=rep(1, nrow(X)), k=11){
+ewknn.predict <- function(X, t, qX, wvec=rep(1, nrow(X)), k=ncol(X)){
 	
 	m = nrow(X)
 	n = ncol(X)
@@ -61,18 +61,29 @@ ewknn.predict <- function(X, t, qX, wvec=rep(1, nrow(X)), k=11){
 	return(out)
 }
 
-preproClncKNN=function(c, survobj){
+
+preproClncKNN=function(c, survobj=NULL, isFactorIn=NULL, dwIn=NULL){
 	n = ncol(c)
 	m = nrow(c)
-	isFactor = rep(NA, n)
-	for(i in 1:n) isFactor[i] = is.factor(c[,i])
-	names(isFactor) = colnames(c)
-	isFactor["grade"] = TRUE
+	if(!is.null(survobj)) if(nrow(survobj) != m) {stop("error: nrow(survobj) != nrow(clnc)")}
+	if(!is.null(dwIn)) if(length(dwIn) != n) stop("error: length(distWeight) != ncol(clnc)")
+	if(!is.null(isFactorIn)) if(length(isFactorIn) != n) stop("error: length(isFactor) != ncol(clnc)")
+	
+	if(is.null(survobj) & is.null(dwIn)) stop("error: distance weight and survival object cannot be both NULL!")
+	if(is.null(isFactorIn)){
+		isFactor = rep(NA, n)
+		for(i in 1:n) isFactor[i] = is.factor(c[,i])
+		names(isFactor) = colnames(c)
+		isFactor["grade"] = TRUE
+	}else{
+		isFactor = isFactorIn
+	}
 	c[,"grade"] = factor(c[,"grade"])
 	distWeight = list()
-	ws = rep(NA, n)
-	names(ws) = colnames(c)
+	ccdi = rep(NA, n)
+	names(ccdi) = colnames(c)
 	for(i in 1:n){
+		#cat(colnames(c)[i], "\n");flush.console()
 		x = c[,i]
 		if(isFactor[i]){
 			max.i = which.max(table(x))
@@ -81,33 +92,81 @@ preproClncKNN=function(c, survobj){
 			contrmat[max.i,] = rep(-1, ncol(contrmat))
 			contrmat[-max.i,] = diag(1, ncol(contrmat-1))
 			contrasts(x) = contrmat
-			fit = coxph(survobj~x)
-			coeff = fit$coef
-			coeff[is.na(coeff)] = 0
-			coeffFac = rep(0, length(levx))
-			coeffFac[-max.i] = coeff
-			coeffFac[max.i] = 0-sum(coeff)
-			coeff = coeffFac
-			names(coeff) = levels(x) 
+			if(!is.null(survobj)){
+				fit = coxph(survobj~x)
+				coeff = fit$coef
+				coeff[is.na(coeff)] = 0
+				coeffFac = rep(0, length(levx))
+				coeffFac[-max.i] = coeff
+				coeffFac[max.i] = 0-sum(coeff)
+				coeff = coeffFac
+				names(coeff) = levels(x)
+			}else{
+				coeff = dwIn[[colnames(c)[i]]]
+			}
+			c[,i] = coeff[c[,i]]
 		}else{
-			fit = coxph(survobj~x)
-			coeff = fit$coef
-			coeff[is.na(coeff)] = 0
+			if(!is.null(survobj)){
+				fit = coxph(survobj~x)
+				coeff = fit$coef
+			}else{
+				coeff = dwIn[[colnames(c)[i]]]
+			}
+			c[,i] = c[,i] * coeff
 		}
-		ws[i] = survConcordance(survobj~predict(fit))$concordance
+		if(!is.null(survobj)) ccdi[i] = survConcordance(survobj~predict(fit))$concordance
 		distWeight[[i]] = coeff
 	}
 	names(distWeight) = colnames(c)
-	out = list(clinical = c, isFactor = isFactor, distWeight = distWeight, weights = ws)
+	if(!is.null(dwIn)) distWeight = dwIn
+	if(!is.null(survobj)) {
+		idx = which(survobj[,2]==1)
+		cout = c[idx,]
+		tout = survobj[idx, 1]
+		idx2 = which(ccdi > 0.55 | ccdi < 0.45)
+		cout = cout[, names(idx2)]
+		ccdi = ccdi[names(idx2)]
+		isFactor = isFactor[names(idx2)]
+		distWeight = distWeight[names(idx2)]
+	}else{
+		cout = c
+		tout = NULL
+	}
+	
+	out = list(clinical = cout, time = tout, concordance=ccdi, isFactor = isFactor, distWeight = distWeight)
 }
 
-clncDist = function(a, b, isFac, dw){
+clncDist = function(a, b, isFac, dw, wvec, minimumDist=0.0001){
 	
-	d = mapply(function(x, y, isFaci, dwi){
-		if(isFaci) abs(dwi[x] - dwi[y])
-		else abs(x - y) * dwi
+	d = mapply(function(x, y, isFaci, dwi, wvec){
+		if(isFaci) abs(dwi[x] - dwi[y])*wvec
+		else abs(as.numeric(x) - as.numeric(y)) * dwi *wvec
 	},
-	a, b, isFac, dw
+	a, b, isFac, dw, wvec
 	)
-	mean(d^2)
+	sum(wvec)/sqrt(sum(d^2 + minimumDist))
 }
+
+getAllClncDist = function(C, qc, isFactor, distWeights, wvec){
+	apply( C, 1, function(c, qc, isFac, dw, wv){
+		clncDist(c, qc, isFac, dw, wv) 
+	}, qc = qc, isFac = isFactor, dw = distWeights, wv = wvec
+	)
+}
+
+ewknn.predict.clnc <- function(preproClnc, qC, wvec=rep(1, length(preproClnc$distWeight)), k=nrow(preproClnc$clinical)){
+	clnc = t(preproClnc$clinical)
+	m = nrow(clnc)
+	n = ncol(clnc)
+	
+	qC = qC[, rownames(clnc)]
+
+	if(ncol(qC) != m){stop("error: ncol(qC) != ncol(clnc)!!")}
+	
+	qC = t(preproClncKNN(qC, isFactorIn = preproClnc$isFactor, dwIn = preproClnc$distWeight)$clinical)
+	nq = ncol(qC)
+	t = .Call("ewknnPredictR2C", clnc, preproClnc$time, qC,wvec, k, m, n, nq)	
+
+	return (t)	
+}
+
