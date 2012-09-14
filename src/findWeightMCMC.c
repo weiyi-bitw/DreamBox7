e8f08ead072c912106282efa3a8e3b1c5df9eeca
weiyi_gisl002 <wc2302@columbia.edu>
2012-09-14 18:04:19 -0400

    building mcmc

--- src/findWeightMCMC.c
+++ src/findWeightMCMC.c
@@ -2,8 +2,42 @@
 #include <stdlib.h>
 #include <R.h>
 #include <Rdefines.h>
+#include "concordanceIndex.h"
+
+void findWeightMCMC(const double* x, const double* surv, double *w, int *mIn, int *nIn, double *dIn, int *maxIterIn){
+	int i,j, count = 0, maxIter = *maxIterIn, m = *mIn, n = *nIn;
+	double* pred = (double*) calloc(n, sizeof(double));
+	double* bestPred = (double*) calloc(n, sizeof(double));
+	double ccdi_cur, ccdi_max, delta = *dIn;
 
 
+	Rprintf("Starting weights: \n");
+	for(i = 0; i < m; i++) Rprintf("\t%f", w[i]);
+	Rprintf("\n");
 
+	for(j = 0; j < n; j++){
+		bestPred[j] = 0;
+		for(i = 0; i < m; i++){
+			bestPred[j] += x[i + j*m] * w[i];
+		}
+	}
 
+	
+	Rprintf("Starting CCDI: \n");
+	ccdi_max = concordance_index(bestPred, surv, n);
+	Rprintf("\t%f", ccdi_max);
+	Rprintf("\n");
+
+	memcpy(pred, bestPred, n*sizeof(double));
+	ccdi_cur = ccdi_max;
+
+	//while(count < maxIter){
+		
+
+	//}
+
+	free(bestPred);
+	free(pred);
+	Rprintf("Leave C function.\n");
+}
 
