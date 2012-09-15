2be32214d2e81ba5b85da51cfe1c5469453e2abb
weiyi <ninpy.weiyi@gmail.com>
2012-09-15 13:33:46 -0400

    sync

--- R/findWeightMCMC.R
+++ R/findWeightMCMC.R
@@ -1,7 +1,8 @@
-fwMCMC = function(x, surv, delta = 0.01, maxIter = 10000){
+fwMCMC = function(x, surv, kmax = 2, delta = 0.01, maxIter = 10000){
 	w = coxph(surv~., data=data.frame(t(x)))$coef
+	w = w/w[1]
 	m = nrow(x)
 	n = ncol(x)
-	out = .C("findWeightMCMC", x=x, surv=surv,w = w, mIn = m, nIn = n, dIn = delta, maxIterIn = maxIter)
+	out = .C("findWeightMCMC", x=as.double(x), surv=as.double(surv),w = as.double(w), mIn = as.integer(m), nIn = as.integer(n),kmaxIn=as.integer(kmax), dIn = as.double(delta), maxIterIn = as.integer(maxIter))
 	return (out)
 }
--- src/findWeightMCMC.c
+++ src/findWeightMCMC.c
@@ -2,13 +2,19 @@
 #include <stdlib.h>
 #include <R.h>
 #include <Rdefines.h>
+#include <time.h>
 #include "concordanceIndex.h"
 
-void findWeightMCMC(const double* x, const double* surv, double *w, int *mIn, int *nIn, double *dIn, int *maxIterIn){
-	int i,j, count = 0, maxIter = *maxIterIn, m = *mIn, n = *nIn;
+void findWeightMCMC(const double* x, const double* surv, double *w, int *mIn, int *nIn, int *kmaxIn, double *dIn, int *maxIterIn){
+	int i,j,k, count = 0, maxIter = *maxIterIn, m = *mIn, n = *nIn;
+	int kMax = *kmaxIn; // number of changed features
+	double* dW = (double*) calloc(kMax, sizeof(double));
+	int* dWi = (int*) calloc(kMax, sizeof(int));
 	double* pred = (double*) calloc(n, sizeof(double));
 	double* bestPred = (double*) calloc(n, sizeof(double));
-	double ccdi_cur, ccdi_max, delta = *dIn;
+	double ccdi_cur, ccdi_max, delta = *dIn, diff, ratio;
+
+	srand(time(NULL));
 
 
 	Rprintf("Starting weights: \n");
@@ -29,13 +35,42 @@ void findWeightMCMC(const double* x, const double* surv, double *w, int *mIn, in
 	Rprintf("\n");
 
 	memcpy(pred, bestPred, n*sizeof(double));
-	ccdi_cur = ccdi_max;
+	//Rprintf("%d\t%d\n", count, maxIter);
+	while(count < maxIter){
+		for(k = 0; k < kMax; k++){
+			i = rand() % (m-1);
+			dWi[k] = i+1;
+			if(rand() & 1) {
+				for(j = 0; j < n; j++) pred[j] += delta * x[(i+1) + j*m];
+				dW[k] = delta;
+			}else{
+				for(j = 0; j < n; j++) pred[j] -= delta * x[(i+1) + j*m];
+				dW[k] = -delta;
+			}
+		}
+		ccdi_cur = concordance_index(pred, surv, n);
+		diff = 100 * (ccdi_cur-0.5) / (ccdi_max-0.5);
+		//Rprintf("%f\n", ccdi_cur);
+		if(rand() % 100 <= diff){
+			Rprintf("New best CCDI: \t%f \n", ccdi_cur);
+			Rprintf("Weights: \n");
+			for(i = 0; i < m; i++) Rprintf("\t%f", w[i]);
+			Rprintf("\n");
+			count = 0;
+			ccdi_max = ccdi_cur;
+			for(k = 0; k < kMax; k++) w[dWi[k]] += dW[k];
+			memcpy(bestPred,pred, n*sizeof(double));
+		}else{
+			memcpy(pred, bestPred, n*sizeof(double));
+		}
 
-	//while(count < maxIter){
-		
 
-	//}
+		count++;
+	}
 
+
+	free(dWi);
+	free(dW);
 	free(bestPred);
 	free(pred);
 	Rprintf("Leave C function.\n");
