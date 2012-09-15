464da984d707d9a6fe66468b9b91d82195fc0687
weiyi_gisl002 <wc2302@columbia.edu>
2012-09-15 16:16:39 -0400

    BF weight finding

--- R/findWeightBF.R
+++ R/findWeightBF.R
@@ -1,8 +1,10 @@
-fwMCMC = function(x, surv, delta = 0.5, maxIter = 10000){
-	w = coxph(surv~., data=data.frame(t(x)))$coef
-	w = w/w[1]
+BFFW = function(x, surv, w=NULL, delta = 0.5, maxIter = 10000, verbose = TRUE){
+	if(is.null(w)){
+		w = coxph(surv~., data=data.frame(t(x)))$coef
+		w = w/w[1]
+	}
 	m = nrow(x)
 	n = ncol(x)
-	out = .C("findWeightBF", x=as.double(x), surv=as.double(surv),w = as.double(w), mIn = as.integer(m), nIn = as.integer(n), dIn = as.double(delta), maxIterIn = as.integer(maxIter))
-	return (out)
+	out = .C("findWeightBF", x=as.double(x), surv=as.double(surv),w = as.double(w), mIn = as.integer(m), nIn = as.integer(n), dIn = as.double(delta), maxIterIn = as.integer(maxIter), verbose = as.integer(verbose))
+	return (out$w)
 }
--- src/findWeightBF.c
+++ src/findWeightBF.c
@@ -5,20 +5,20 @@
 #include <time.h>
 #include "concordanceIndex.h"
 
-void findWeightBF(const double* x, const double* surv, double *w, int *mIn, int *nIn, double *dIn, int *maxIterIn){
-	int i,j,k, count = 0, maxIter = *maxIterIn, m = *mIn, n = *nIn, dice;
+void findWeightBF(const double* x, const double* surv, double *w, int *mIn, int *nIn, double *dIn, int *maxIterIn, int *verbose){
+	int i,j,k, count = 0, maxIter = *maxIterIn, m = *mIn, n = *nIn, streak = 0;
 	double* dW = (double*) calloc(m-1, sizeof(double));
 	double* pred = (double*) calloc(n, sizeof(double));
 	double* bestPred = (double*) calloc(n, sizeof(double));
-	double ccdi_cur, ccdi_max, delta_max = *dIn, diff, ratio;
+	double ccdi_cur, ccdi_max, delta_max = *dIn, ratio;
 
 	srand(time(NULL));
 
-
+	/*
 	Rprintf("Starting weights: \n");
 	for(i = 0; i < m; i++) Rprintf("\t%f", w[i]);
 	Rprintf("\n");
-
+	*/
 	for(j = 0; j < n; j++){
 		bestPred[j] = 0;
 		for(i = 0; i < m; i++){
@@ -27,26 +27,24 @@ void findWeightBF(const double* x, const double* surv, double *w, int *mIn, int 
 	}
 
 	
-	Rprintf("Starting CCDI: \n");
 	ccdi_max = concordance_index(bestPred, surv, n);
-	Rprintf("\t%f", ccdi_max);
-	Rprintf("\n");
+	Rprintf("Starting CCDI: \t%f\n", ccdi_max);
 
 	memcpy(pred, bestPred, n*sizeof(double));
 	//Rprintf("%d\t%d\n", count, maxIter);
 	while(count < maxIter){
 		for(k = 1; k < m; k++){
 			//ratio = (double) (rand() % 100) / 100;
-			ratio = (double) (rand() % 200 - 100) / 100;
-			dice = rand() % 3;
-			if(dice == 2) {
+			ratio = (double) (rand() % 100 - 50) / 100;
+			//dice = rand() % 3;
+			if(rand() & 1) {
 				//dW[k-1] = delta_max * ratio;
 				dW[k-1] = ratio * w[k];
 				for(j = 0; j < n; j++) pred[j] += dW[k-1] * x[(k) + j*m];
-			}else if(dice == 1){
+			//}else if(dice == 1){
 				//dW[k-1] = -delta_max * ratio;
-				dW[k-1] = ratio * w[k];
-				for(j = 0; j < n; j++) pred[j] += dW[k-1] * x[(k) + j*m];
+			//	dW[k-1] = ratio * w[k];
+			//	for(j = 0; j < n; j++) pred[j] += dW[k-1] * x[(k) + j*m];
 			}else{
 				dW[k-1] = 0;
 			}
@@ -55,28 +53,40 @@ void findWeightBF(const double* x, const double* surv, double *w, int *mIn, int 
 		ccdi_cur = concordance_index(pred, surv, n);
 		//Rprintf("%f\n", ccdi_cur);
 		if(ccdi_cur > ccdi_max){
-			Rprintf("New best CCDI: \t%f \n", ccdi_cur);
-			count = 0;
 			ccdi_max = ccdi_cur;
 			for(k = 1; k < m; k++) w[k] += dW[k-1];
 			
-			Rprintf("Weights: \n");
-			for(i = 0; i < m; i++) Rprintf("\t%f", w[i]);
-			Rprintf("\n");
-			
+			if(*verbose == 1){
+				Rprintf("New best CCDI: \t%f\t Streak: \t%d \n", ccdi_max, count);
+				Rprintf("Weights: \n");
+				for(i = 0; i < m; i++) Rprintf("\t%f", w[i]);
+				Rprintf("\n");
+			}
+
+			if(count > streak) streak = count;
+			count = 0;
 			memcpy(bestPred,pred, n*sizeof(double));
 		}else{
 			memcpy(pred, bestPred, n*sizeof(double));
 		}
 
-		if(count % 1000 == 0) Rprintf("%d\n", count);
+		//if(count % 1000 == 0) Rprintf("%d\n", count);
 		count++;
 	}
+
 
 
 	free(dW);
 	free(bestPred);
 	free(pred);
-	Rprintf("Leave C function.\n");
+	
+	Rprintf("================\n");
+	Rprintf("Best CCDI found: \t%f \n", ccdi_max);
+	Rprintf("Weights: \n");
+	for(i = 0; i < m; i++) Rprintf("\t%f", w[i]);
+	Rprintf("\n");
+	Rprintf("\nLongest streak: \t%d \n", streak);
+	Rprintf("================\n");
+	//Rprintf("Leave C function.\n");
 }
 
