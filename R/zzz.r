3c7d118057baab8c45ec3d00a100a1687f278207
weiyi <ninpy.weiyi@gmail.com>
2012-09-24 22:14:45 -0400

    fix

--- R/zzz.r
+++ R/zzz.r
@@ -9,7 +9,15 @@
 	"===============================\n",
 	"\n",
 	"Welcome to DreamBox7 package!\n\n",
-	" -- Brought to you by Wei-Yi Cheng and Team Attractor Metagenes.\n",
+	" -- Brought to you by Wei-Yi Cheng and Team Attractor Metagenes.\n\n",
+	"===============================\n\n",
+	"Aren't Attractor Metagenes awesome? If you agree, please recommend our paper at  \n",
+	"http://arxiv.org/abs/1204.6538 to everybody and help us further spread the idea!\n",
+	"We hope one day cancer patients can be profiled by several metagenes indicating \n",
+	"important biomolecular events of the disease such as chromosomal instability, \n",
+	"invasiveness, or lymphocyte responses, instead of mutually exclusive subtypes.\n",
+	"This may lead to a more comprehensive understanding of cancer, a better prognosis,\n ",
+	"and even new druggable targets! Let's get it started! #RiseAboveCancer\n",
 	"\n"
 	)
 }
